6a3b3ddb9a749c95d5aa80f97ce0b62492ba2e06
orangegrove_1955 <orangegrove_1955@hotmail.com>
2020-05-27 08:15:57 +1000

    Added canvas element, and ability to draw detections onto it

--- script.js
+++ script.js
@@ -23,17 +23,41 @@ Promise.all([
 
 // Add event listener for start of video to call detect faces every 100 milliseconds
 videoElement.addEventListener("play", () => {
-  setInterval(detectFace, 100);
+  const { canvas, displaySize } = createCanvas();
+  setInterval(() => detectFace(canvas, displaySize), 100);
 });
 
-/** Use faceapi to detect all faces in video and return landmark and expressions from them */
-const detectFace = async () => {
+/** Create a canvas element for faceapi to draw detections on */
+const createCanvas = () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+
+  // Set size of canvas
+  const displaySize = { width: video.width, height: video.height };
+
+  // Resize dimensions in faceapi to match canvas
+  faceapi.matchDimensions(canvas, displaySize);
+
+  return { canvas, displaySize };
+};
+
+/** Use faceapi to detect all faces in video and display landmark and expressions from them on canvas
+ * @param canvas Canvas element to display detections
+ * @param displaySize Size of display to overlay video detections on
+ */
+const detectFace = async (canvas, displaySize) => {
   const detections = await faceapi
     // Detect all faces using Tiny face detector, using no custom options
     .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-    // Extract landmarks from faces found
     .withFaceLandmarks()
-    // Determine expression of faces found
     .withFaceExpressions();
-  console.log(detections);
+
+  // Display detections on resized canvas
+  const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+  // Clear canvas and draw detections, landmarks and expressions
+  canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+  faceapi.draw.drawDetections(canvas, resizedDetections);
+  faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+  faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
 };
