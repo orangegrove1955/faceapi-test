9cedd3178aa11224502901c391d3a3d2b9bbde72
orangegrove_1955 <orangegrove_1955@hotmail.com>
2020-05-27 07:58:25 +1000

    Create detections and console log results

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ Promise.all([
 ])
   // Begin showing video on page
   .then(startVideo);
+
+// Add event listener for start of video to call detect faces every 100 milliseconds
+videoElement.addEventListener("play", () => {
+  setInterval(detectFace, 100);
+});
+
+/** Use faceapi to detect all faces in video and return landmark and expressions from them */
+const detectFace = async () => {
+  const detections = await faceapi
+    // Detect all faces using Tiny face detector, using no custom options
+    .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+    // Extract landmarks from faces found
+    .withFaceLandmarks()
+    // Determine expression of faces found
+    .withFaceExpressions();
+  console.log(detections);
+};
